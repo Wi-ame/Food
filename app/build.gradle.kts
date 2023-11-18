9e467b5bd155d73dbaa1ef0a82d6e7a841adf428
WIAME <wiwimess92@gmail.com>
2023-11-18 18:09:01 +0100

    change1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.cscorner.food"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cscorner.food"
@@ -34,7 +34,7 @@ android {
         jvmTarget = "1.8"
     }
 }
-
+val nav_version = "2.7.5"
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -44,4 +44,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    //intuit
+    implementation ("com.intuit.sdp:sdp-android:1.0.6")
+    implementation ("com.intuit.ssp:ssp-android:1.0.6")
+    //gif
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
+// retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.3.0")
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
 }
--- app/src/main/java/com/cscorner/food/MainActivity.kt
+++ app/src/main/java/com/cscorner/food/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cscorner.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ButtonNavigation =findViewById<BottomNavigationView>(R.id.btn_nav)
+        val NavController= Navigation.findNavController( this ,R.id.host_fragment)
+        NavigationUI.setupWithNavController(ButtonNavigation,NavController)
     }
 }
